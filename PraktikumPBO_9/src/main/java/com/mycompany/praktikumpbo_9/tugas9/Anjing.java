6ede09701834ed24f2a1b5f49638318ec9d0a5f8
Chanif <chanifg19@gmail.com>
2025-12-04 21:51:00 +0700

    Hiyalaa

---
+++ PraktikumPBO_9/src/main/java/com/mycompany/praktikumpbo_9/tugas9/Anjing.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.praktikumpbo_9.tugas9;
+
+/**
+ *
+ * @author aniip
+ */
+class Anjing extends Hewan {
+    @Override
+    void suara() {
+        System.out.println("Anjing mengeluarkan suara: Guk Guk");
+    }
+}
---
+++ PraktikumPBO_9/src/main/java/com/mycompany/praktikumpbo_9/tugas9/Kucing.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.praktikumpbo_9.tugas9;
+
+/**
+ *
+ * @author aniip
+ */
+class Kucing extends Hewan {
+    @Override
+    void suara() {
+        System.out.println("Kucing mengeluarkan suara: Meong");
+    }
+}
+
